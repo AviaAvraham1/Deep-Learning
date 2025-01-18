d82d2e6e8650e856197d703a7dbc87d2b17a2beb
Daniel Perez <daniel.pe@cs.technion.ac.il>
2025-01-18 10:24:14 +0200

    part 1 - almost done

--- hw3/hw3/answers.py
+++ hw3/hw3/answers.py
@@ -11,18 +11,17 @@ math (delimited with $$).
 
 def part1_rnn_hyperparams():
     hypers = dict(
-        batch_size=0,
-        seq_len=0,
-        h_dim=0,
-        n_layers=0,
-        dropout=0,
-        learn_rate=0.0,
-        lr_sched_factor=0.0,
-        lr_sched_patience=0,
+        batch_size=64,
+        seq_len=100,
+        h_dim=512,
+        n_layers=3,
+        dropout=0.3,
+        learn_rate=0.001,
+        lr_sched_factor=0.1,
+        lr_sched_patience=3,
     )
-    # TODO: Set the hyperparameters to train the model.
+    # Set the hyperparameters to train the model.
     # ====== YOUR CODE: ======
-    
     # ========================
     return hypers
 
@@ -30,30 +29,48 @@ def part1_rnn_hyperparams():
 def part1_generation_params():
     start_seq = ""
     temperature = 0.0001
-    # TODO: Tweak the parameters to generate a literary masterpiece.
+    # Tweak the parameters to generate a literary masterpiece.
     # ====== YOUR CODE: ======
-    
     # ========================
     return start_seq, temperature
 
 
 part1_q1 = r"""
-**Your answer:**
-
+We split the corpus into sequences because if we train for the whole text, we'll have to 
+keep a lot of memory for long text (to save all the hidden-states in order to use later
+in the backpropagation), as studied in class. So by splitting it to chunks, we can run forward and backward
+through chunks of sequences, and thus only need to keep the hidden-states of the last layer
+in each chunk. Also, splitting to chunks allows to speed up training using parallelism.
 """
 
 part1_q2 = r"""
-**Your answer:**
+The model's memory is longer than the sequence length since the hidden state are carried over between sequences
+and so they serve as a sort of long-term memory that is maintained throughout the entire training text.
+Also, the GRU architecture is built to allow retaining important information over longer periods and forgetting
+irrelevant information.
 
 """
 
 part1_q3 = r"""
-**Your answer:**
+We don't shuffle the order of batches when training because if we did, we'll lose important information
+regarding the position of that sequence in the text. We don't actually want to refer to each such sequence
+as an independant sample, but we want to preserve their order to be able to learn their sequential dependencies.
+(The importance of the order between batches is reflected in the value of the hidden state, which retains information
+about previous sequences and influences the prediction of subsequent ones.)
 
 """
 
 part1_q4 = r"""
-**Your answer:**
+1. When sampling, we would prefer to control the distributions and make them less uniform to increase the chance of
+    sampling the char(s) with the highest scores compared to the others.
+2. When the temperature is very high, the softmax which takes the scores and returns probablities, 
+    will return probabilities that are very close to uniform distribution, meaning the predictions of the models
+    are similar to randomly picking a char from the vocabulary each time, as if we didn't learn anything from the 
+    training data.
+3. When the temperature is very low, the softmax will exaggerate the differences between the different scores, i.e 
+    sharpen the probability distribution, making the model highly confident about the most likely characters while
+    practically ignoring the ones with the lower scores. So lower number of characters likely to be predicted, but 
+    these are predicted with higher probability.
 
 
 """
@@ -63,7 +80,7 @@ part1_q4 = r"""
 # ==============
 # Part 2 answers
 
-PART2_CUSTOM_DATA_URL = None
+PART2_CUSTOM_DATA_URL = "https://github.com/AviaAvraham1/TempDatasets/raw/refs/heads/main/George_W_Bush2.zip"
 
 
 def part2_vae_hyperparams():
@@ -77,7 +94,7 @@ def part2_vae_hyperparams():
     hypers["z_dim"] = 64
     hypers["x_sigma2"] = 0.1
     hypers["learn_rate"] = 0.0002
-    hypers["betas"] = #(a,b)
+    hypers["betas"] = 0 #(a,b)
     # ========================
     return hypers
 
--- hw3/hw3/charnn.py
+++ hw3/hw3/charnn.py
@@ -18,12 +18,13 @@ def char_maps(text: str):
         represented by it. The reverse of the above map.
 
     """
-    # TODO:
     #  Create two maps as described in the docstring above.
     #  It's best if you also sort the chars before assigning indices, so that
     #  they're in lexical order.
     # ====== YOUR CODE: ======
-    raise NotImplementedError()
+    all_unique_chars = sorted(set(text)) # extracts the characters
+    char_to_idx = {char: idx for idx, char in enumerate(all_unique_chars)}
+    idx_to_char = {idx: char for char, idx in char_to_idx.items()}
     # ========================
     return char_to_idx, idx_to_char
 
@@ -37,9 +38,16 @@ def remove_chars(text: str, chars_to_remove):
         - text_clean: the text after removing the chars.
         - n_removed: Number of chars removed.
     """
-    # TODO: Implement according to the docstring.
+    # Implement according to the docstring.
     # ====== YOUR CODE: ======
-    raise NotImplementedError()
+    text_clean = [] # will append here the wanted chars, then turn it into a string
+    n_removed = 0
+    for char in text:
+        if char in chars_to_remove:
+            n_removed += 1 
+        else:
+            text_clean.append(char)
+    text_clean = ''.join(text_clean)
     # ========================
     return text_clean, n_removed
 
@@ -57,9 +65,16 @@ def chars_to_onehot(text: str, char_to_idx: dict) -> Tensor:
     and D is the number of unique chars in the sequence. The dtype of the
     returned tensor will be torch.int8.
     """
-    # TODO: Implement the embedding.
+    # Implement the embedding.
     # ====== YOUR CODE: ======
-    raise NotImplementedError()
+    D = len(char_to_idx)
+    N = len(text)
+    result = torch.zeros((N, D), dtype=torch.int8)
+    for i, char in enumerate(text):
+        if char in char_to_idx: 
+            idx = char_to_idx[char]
+            result[i, idx] = 1
+    # result is a tensor where each col is the encoding of the char in that positon in the text
     # ========================
     return result
 
@@ -74,9 +89,13 @@ def onehot_to_chars(embedded_text: Tensor, idx_to_char: dict) -> str:
     :return: A string containing the text sequence represented by the
     embedding.
     """
-    # TODO: Implement the reverse-embedding.
+    # Implement the reverse-embedding.
     # ====== YOUR CODE: ======
-    raise NotImplementedError()
+    result = []
+    for row in embedded_text:
+        char_idx = row.argmax().item() # get the index where there is a 1
+        result.append(idx_to_char[char_idx]) # and map it to it's corresponding char
+    result = ''.join(result)
     # ========================
     return result
 
@@ -97,7 +116,6 @@ def chars_to_labelled_samples(text: str, char_to_idx: dict, seq_len: int, device
     the number of created samples, S is the seq_len and V is the embedding
     dimension.
     """
-    # TODO:
     #  Implement the labelled samples creation.
     #  1. Embed the given text.
     #  2. Create the samples tensor by splitting to groups of seq_len.
@@ -105,7 +123,13 @@ def chars_to_labelled_samples(text: str, char_to_idx: dict, seq_len: int, device
     #  3. Create the labels tensor in a similar way and convert to indices.
     #  Note that no explicit loops are required to implement this function.
     # ====== YOUR CODE: ======
-    raise NotImplementedError()
+    embedded_text = chars_to_onehot(text, char_to_idx).to(device)
+    num_samples = len(text) // seq_len
+    samples = embedded_text[:num_samples * seq_len].reshape(num_samples, seq_len, -1)
+    # the label of each char is the next char, this is gained by shifting the text by 1:
+    labels = torch.tensor([char_to_idx[char] for char in text[1:]]).to(device) 
+    # align to fit the number of samples (ignore the last char (has no next char))
+    labels = labels[:num_samples * seq_len].reshape(num_samples, seq_len)
     # ========================
     return samples, labels
 
@@ -119,9 +143,12 @@ def hot_softmax(y, dim=0, temperature=1.0):
     :param temperature: Temperature.
     :return: Softmax computed with the temperature parameter.
     """
-    # TODO: Implement based on the above.
+    # Implement based on the above.
     # ====== YOUR CODE: ======
-    raise NotImplementedError()
+    # scale the input (which is the model scores) by the temperature
+    # (lower t means less uniform distribution and vise versa)
+    scaled_input = y / temperature
+    result = torch.softmax(scaled_input, dim=dim) # the probablities
     # ========================
     return result
 
@@ -157,7 +184,18 @@ def generate_from_model(model, start_sequence, n_chars, char_maps, T):
     #  necessary for this. Best to disable tracking for speed.
     #  See torch.no_grad().
     # ====== YOUR CODE: ======
-    raise NotImplementedError()
+    with torch.no_grad():
+        # model.eval()
+        hidden_state = None
+        out = out_text
+        for _ in range(n_chars - len(start_sequence)):
+            input_sequence = chars_to_onehot(out, char_to_idx).unsqueeze(0).to(device=device, dtype=torch.float)
+            output_sequence, hidden_state = model(input_sequence, hidden_state)
+            output_probs = hot_softmax(output_sequence.squeeze(0)[-1], temperature=T)
+            next_char_idx = torch.multinomial(output_probs, 1).item()
+            next_char = idx_to_char[next_char_idx]
+            out = next_char
+            out_text += out
     # ========================
 
     return out_text
@@ -180,7 +218,6 @@ class SequenceBatchSampler(torch.utils.data.Sampler):
         self.batch_size = batch_size
 
     def __iter__(self) -> Iterator[int]:
-        # TODO:
         #  Return an iterator of indices, i.e. numbers in range(len(dataset)).
         #  dataset and represents one  batch.
         #  The indices must be generated in a way that ensures
@@ -190,7 +227,10 @@ class SequenceBatchSampler(torch.utils.data.Sampler):
         #  you can drop it.
         idx = None  # idx should be a 1-d list of indices.
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        num_batches = len(self.dataset) // self.batch_size
+        idx = torch.arange(num_batches * self.batch_size)
+        idx = idx.view(self.batch_size, num_batches).t().reshape(-1)
+        idx = idx.tolist()
         # ========================
         return iter(idx)
 
@@ -219,12 +259,24 @@ class MultilayerGRU(nn.Module):
         self.out_dim = out_dim
         self.h_dim = h_dim
         self.n_layers = n_layers
-        self.layer_params = []
-
-        # TODO: READ THIS SECTION!!
+        # self.layer_params = []
 
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        self.dropout = nn.Dropout(dropout)
+        self.layer_params = nn.ModuleList()
+        layer_in_dim = self.in_dim
+        for i in range(self.n_layers):
+            layer_param = nn.ModuleDict({
+            "wxz": nn.Linear(layer_in_dim, self.h_dim, bias=True),
+            "whz": nn.Linear(self.h_dim, self.h_dim, bias=False),
+            "wxr": nn.Linear(layer_in_dim, self.h_dim, bias=True),
+            "whr": nn.Linear(self.h_dim, self.h_dim, bias=False),
+            "wxg": nn.Linear(layer_in_dim, self.h_dim, bias=True),
+            "whg": nn.Linear(self.h_dim, self.h_dim, bias=False),
+            })
+            self.layer_params.append(layer_param)
+            layer_in_dim = self.h_dim
+        self.output_layer = nn.Linear(self.h_dim, self.out_dim, bias=True)
         # ========================
 
     def forward(self, input: Tensor, hidden_state: Tensor = None):
@@ -256,9 +308,40 @@ class MultilayerGRU(nn.Module):
         layer_input = input
         layer_output = None
 
-        # TODO: READ THIS SECTION!!
         # ====== YOUR CODE: ======
-        # Loop over layers of the model
-        raise NotImplementedError()
+        for layer_idx in range(self.n_layers):
+            # move all layer parameters to the device
+            wxz = self.layer_params[layer_idx]["wxz"].to(input.device)
+            whz = self.layer_params[layer_idx]["whz"].to(input.device)
+            wxr = self.layer_params[layer_idx]["wxr"].to(input.device)
+            whr = self.layer_params[layer_idx]["whr"].to(input.device)
+            wxg = self.layer_params[layer_idx]["wxg"].to(input.device)
+            whg = self.layer_params[layer_idx]["whg"].to(input.device)
+
+            h_t = layer_states[layer_idx] # (B,H)
+
+            # loop over the current sequence (t) to build h_t and append to layer_output
+            layer_outputs = []
+            for i in range(seq_len):
+                x_t = layer_input[:, i, :] # (B, V) or (B, H)
+                z_t = torch.sigmoid(wxz(x_t) + whz(h_t))
+                r_t = torch.sigmoid(wxr(x_t) + whr(h_t))
+                g_t = torch.tanh(wxg(x_t) + whg(r_t * h_t))
+                h_t = z_t * h_t + (1 - z_t) * g_t
+                layer_outputs.append(h_t) 
+            
+            # save last h_t as layer state (goes to next layer)
+            layer_states[layer_idx] = h_t
+
+            # stack all the output layers to one output tensor
+            layer_output = torch.stack(layer_outputs, dim=1)  # (B, S, H)
+
+            # and prepare it as input for the next layer
+            layer_input = self.dropout(layer_output)
+
+        # final fully-connected to map the output of the last layer to the output space
+        layer_output = self.output_layer(layer_input)  # (B, S, O) # applies a linear transformation
+        # stacks the final hidden states from all layers into a single tensor:
+        hidden_state = torch.stack(layer_states, dim=1)  # (B, L, H) 
         # ========================
         return layer_output, hidden_state
