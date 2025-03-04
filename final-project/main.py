8fa48619dd73ba653dece21f4f3f31a791c3b7fb
Daniel Perez <daniel.pe@cs.technion.ac.il>
2025-03-04 15:02:31 +0200

    improvment

--- final-project/main.py
+++ final-project/main.py
@@ -56,14 +56,14 @@ if __name__ == "__main__":
                                             classifier_filename="frozen_contrastive_encoder_classifier.pt", 
                                             log_filename="frozen_contrastive_encoder_classifier.log")
         else:
-            # decoder = Decoder(latent_dim=args.latent_dim).to(args.device)
-            # print("Training Self-Supervised Autoencoder...")
-            # train_autoencoder(encoder, decoder, train_loader, val_loader, test_loader, args, 
-            #                     encoder_filename="encoder_frozen.pt",
-            #                     decoder_filename="trained_decoder.pt",
-            #                     log_filename="frozen_autoencoder.log")
-            # plot_tsne(encoder, test_loader, args.device)
-            encoder = load_model(encoder, "models/CIFAR10_encoder_frozen.pt", args.device)
+            decoder = Decoder(latent_dim=args.latent_dim).to(args.device)
+            print("Training Self-Supervised Autoencoder...")
+            train_autoencoder(encoder, decoder, train_loader, val_loader, test_loader, args, 
+                                encoder_filename="encoder_frozen.pt",
+                                decoder_filename="trained_decoder.pt",
+                                log_filename="frozen_autoencoder.log")
+            plot_tsne(encoder, test_loader, args.device)
+            # encoder = load_model(encoder, "models/CIFAR10_encoder_frozen.pt", args.device)
             print("Training Classifier on Frozen Encoder...")
             train_classifier_on_frozen_encoder(encoder, classifier, train_loader, val_loader, test_loader, args, 
                                             classifier_filename="frozen_encoder_classifier.pt", 
